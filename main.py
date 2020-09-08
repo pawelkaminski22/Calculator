164da42cf557d4e47c0514efdd985b32cf8241d7
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-09-08 16:53:39 +0200

    Add multi values

--- main.py
+++ main.py
@@ -1,36 +1,59 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+values = []
+
 
 def dodawanie(value_1, value_2):
     print(value_1 + value_2)
 
+
 def odejmowanie(value_1, value_2):
     print(value_1 - value_2)
 
-def mnozenie(value_1, value_2):
-    print(value_1 * value_2)
 
-def dzielenie(value_1, value_2):
-    print(value_1 / value_2)
+def mnozenie(values):
+    result = 1
+    for v in values:
+        result = result * int(v)
+    print(f'Wynik to {result}')
+
+
+def dzielenie(values):
+    result = int(values[0])
+    del values[0]
+    for v in values:
+        result = result / int(v)
+    print(f'Wynik to {result}')
+
 
 if __name__ == "__main__":
     calculation_type = input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:')
-    value_1 = input('podaj pierwszą wartosc: ')
-    value_2 = input('podaj drugą wartosc: ')
+    if calculation_type == '1' or calculation_type == '2':
+        value_1 = input('Podaj pierwszą wartosc: ')
+        value_2 = input('Podaj drugą wartosc: ')
 
-    if value_1.isdigit() and value_2.isdigit():
-        if calculation_type == '1':
-         logging.info('Dodaję ' + value_1 + " i " + value_2)
-         dodawanie(int(value_1), int(value_2))
-        elif calculation_type == '2':
-         logging.info('Odejmuje ' + value_1 + " i " + value_2)
-         odejmowanie(int(value_1), int(value_2))
-        elif calculation_type == '3':
-         logging.info('Mnożę ' + value_1 + " razy " + value_2)
-         mnozenie(int(value_1), int(value_2))
-        elif calculation_type == '4':
-         logging.info('Dzielę ' + value_1 + " przez " + value_2)
-         dzielenie(int(value_1), int(value_2))
-    else:
-        print('You entered non numeric values!')
+        if value_1.isdigit() and value_2.isdigit():
+            if calculation_type == '1':
+                logging.info('Dodaję ' + value_1 + " i " + value_2)
+                dodawanie(int(value_1), int(value_2))
+            elif calculation_type == '2':
+                logging.info('Odejmuje od ' + value_1 + " liczbę " + value_2)
+                odejmowanie(int(value_1), int(value_2))
+        else:
+            print('Tylko wartosci liczbowe!')
+
+    if calculation_type == '3' or calculation_type == '4':
+        while True:
+            value = input('Podaj liczbę lub wpisz Y zeby obliczyc: ')
+            if value == str.lower('y'):
+                break
+            else:
+                values.append(value)
+        if len(values) < 2:
+            print('Podaj przynajmniej dwie liczby')
+        else:
+            if calculation_type == '3':
+                mnozenie(values)
+            elif calculation_type == '4':
+                dzielenie(values)
